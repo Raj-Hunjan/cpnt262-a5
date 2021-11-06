511c9b5c41e11012357167cbbbaeff55be6f81a9
Raj-Hunjan <rajhunjann@gmail.com>
2021-11-05 20:14:52 -0600

    fixed some stuff

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,22 @@
 const cars = require("../data/cars")
-// Define seed data
-// Your data here
 
   
   // Dynamic JSON Endpoint
-  app.get('/api/cars', (req, res) => {
-          res.send(cars);
-      })
-  
-  app.get('/api/cars/:id', (req, res) => {
-          const foundCar = 'Array.find()'; // Use Array.find() here
-          res.send(foundCar);
-      })
+app.get('/api/cars', (req, res) => {
+  res.send(cars);
+})
 
+app.get('/api/cars/:id', (request, response) => {
+
+    const carRandom = cars.find((item) => {
+      if (request.params.id === item.id) {
+        return true;
+      } else {
+        return false;
+      }
+    }); // Use Array. find() here
+    res.send(carRandom);
+  })
+
+
+      
